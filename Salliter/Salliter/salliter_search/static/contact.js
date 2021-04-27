09f981052bad1aa192636cf09c946b3f71cada61
Z004LC8 <Pashupati.Verma@target.com>
2021-04-27 19:31:01 +0530

    salliter

--- Salliter/Salliter/salliter_search/static/contact.js
+++
@@ -1,8 +0,0 @@
-$('#contact').on('submit', function(e){
-        e.preventDefault();
-        $.ajax({
-          success: function() {
-            alert("Thanks for Your comment!");
-          }
-        });
-      });
